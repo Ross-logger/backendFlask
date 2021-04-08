57096260c4d296bac60a119ed232713aee20b264
Ross-logger <75065023+Ross-logger@users.noreply.github.com>
2021-04-08 20:09:32 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgres://oomzugrngzshgq:07ddf913052409e8ffb5fe1451a28504244e269ee506ffab18818a6ed513edc9@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/d6rbu51aggngta'
+SQLALCHEMY_DATABASE_URL = 'postgresql://oomzugrngzshgq:07ddf913052409e8ffb5fe1451a28504244e269ee506ffab18818a6ed513edc9@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/d6rbu51aggngta'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
